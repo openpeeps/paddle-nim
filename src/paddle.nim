1b1b7a499bbedf5a994775f976f17efa3728eaa1
George Lemon <georgelemon@protonmail.com>
2026-05-03 23:12:20 +0300

    update
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

---
+++ src/paddle.nim
@@ -0,0 +1,9 @@
+# A Nim client for the Paddle API
+# https://developer.paddle.com/api-reference/
+#
+# (c) 2026 George Lemon | MIT License
+#          Made by Humans from OpenPeeps
+#          https://github.com/openpeeps/paddle-nim
+
+import ./paddle/[api, products, prices, customers, subscriptions]
+export api, products, prices, customers, subscriptions
